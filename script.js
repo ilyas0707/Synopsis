8e3177c707d8afd4373a34d694b5c2a35993d120
adiletturdakunov2002@gmail.com <ilyas.yangurazov.iy@gmail.com>
2021-09-24 06:16:19 +0600

    update

--- script.js
+++ script.js
@@ -16,12 +16,14 @@ for (i = 0; i < acc.length; i++) {
 
 let d = document
 let menu = d.getElementById('menu'),
+    main = d.getElementById('main')
     slider = d.getElementById('slider')
 
 // console.log(checkbox.getAttribute('checked'))
 
 slider.addEventListener('click', () => {
     menu.classList.toggle('active')
+    main.classList.toggle('active')
 })
 
 let merchantInput = d.getElementById('merchantInput'),
