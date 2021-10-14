180214d85fc8926ef770df95ac0eb6e690be6ba6
adiletturdakunov2002@gmail.com <ilyas.yangurazov.iy@gmail.com>
2021-10-15 02:51:25 +0600

    final

--- final.js
+++ final.js
@@ -1,17 +1,16 @@
-var acc = document.getElementsByClassName('accordion')
-var i
+var acc = document.getElementsByClassName("accordion");
+var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', function () {
-        this.classList.toggle('active')
-
-        var panel = this.nextElementSibling
-        if (panel.style.display === 'grid') {
-            panel.style.display = 'none'
-        } else {
-            panel.style.display = 'grid'
-        }
-    })
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
 
 let d = document
@@ -25,3 +24,19 @@ slider.addEventListener('click', () => {
     menu.classList.toggle('active')
     main.classList.toggle('active')
 })
+
+let openModal = d.getElementsByClassName('open_modal'),
+    modal = d.getElementById('modal'),
+    closeModal = d.getElementsByClassName('close_modal')
+
+for (i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener('click', function () {
+        modal.classList.add('active')
+    })
+}
+
+for (i = 0; i < closeModal.length; i++) {
+    closeModal[i].addEventListener('click', function () {
+        modal.classList.remove('active')
+    })
+}
